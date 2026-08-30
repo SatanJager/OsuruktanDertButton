8890c106af8fdb55d9d2f436912fe0982cf3edc5
Taylan Alp Muhur <satanjager@gmail.com>
2026-08-30 18:22:00 +0200

    InitializeUi bitti

--- MainForm.cs
+++ MainForm.cs
@@ -9,7 +9,7 @@ namespace OsuruktanDertButton
         private ComboBox _languageComboBox = null!;
         private Label _languageLabel = null!;
         private Label _promptLabel = null!;
-        private TextBox _complantTextBox = null!;
+        private TextBox _complaintTextBox = null!;
         private Button _solveButton = null!;
         private Button _historyButton = null!;
         private Label _resultLabel = null!;
@@ -51,10 +51,60 @@ namespace OsuruktanDertButton
             _languageComboBox.SelectedIndex = 0;
             _languageComboBox.SelectedIndexChanged += OnLanguageChanged; // kullanıcı listeden birşey seçtiğinde OnLanguageChanged metodu çalışacak
 
+            _promptLabel = new Label
+            {
+                Left = 20,
+                Top = 55,
+                Width = 430,
+                AutoSize = true,
+            };
 
+            _complaintTextBox = new TextBox
+            {
+                Left = 20,
+                Top = 80,
+                Width = 430,
+                Height = 120,
+                Multiline = true,  //TextBox'ı multiline yaptık
+                ScrollBars = ScrollBars.Vertical, //Kullanıcı çok uzun bir yazı yazarsa kaydırma çubuğu çıksın
+            };
 
+            _solveButton = new Button
+            {
+                Left = 20,
+                Top = 215,
+                Width = 200,
+                Height = 40,
+            };
+            _solveButton.Click += OnSolveClicked; //SolveButton'a basılınca OnSolveClicked metodu başlayacak
 
+            _historyButton = new Button
+            {
+                Left = 250,
+                Top = 215,
+                Width= 200,
+                Height = 40,
+            };
+            _historyButton.Click += OnHistoryClicked; //HistoryButton basıldığında OnHistoryClicked metodu başlayacak
 
+            _resultLabel = new Label
+            {
+                Left = 20,
+                Top = 270,
+                Width = 430,
+                Height = 60,
+                Font = new Font(Font.FontFamily, 10, FontStyle.Bold),  //Varsayılan sistem fontu
+                ForeColor = Color.DarkRed,
+            };
+
+            //Yaratılan nesneleri (RAM' yarattığımız new'leri) ekrana basıyoruz.
+            Controls.Add(_languageLabel);
+            Controls.Add(_languageComboBox);
+            Controls.Add(_promptLabel);
+            Controls.Add(_complaintTextBox);
+            Controls.Add(_solveButton);
+            Controls.Add(_historyButton);
+            Controls.Add(_resultLabel);
         }
 
 
