b10256364fa39c66a90fb0da7829e887b901396b
Taylan Alp Muhur <satanjager@gmail.com>
2026-08-30 18:04:55 +0200

    MainForm'a devam.

--- MainForm.cs
+++ MainForm.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.ComponentModel.Design.Serialization;
 namespace OsuruktanDertButton
 {
     public partial class MainForm : Form
     {
         private Language _currentLanguage = Language.Turkish;
 
-        private ComboBox _languageCombobox = null!;
+        private ComboBox _languageComboBox = null!;
         private Label _languageLabel = null!;
         private Label _promptLabel = null!;
         private TextBox _complantTextBox = null!;
@@ -21,7 +22,39 @@ namespace OsuruktanDertButton
 
         private void InitializeUi()
         {
-            throw new NotImplementedException();
+            Text = Localization.Get(Localization.WindowTitle, _currentLanguage);  //Percerenin başlık çubuğunda gözüke yazı
+            Width = 480;
+            Height = 380;
+            StartPosition = FormStartPosition.CenterScreen; //Percere açılınca ekranın ortasında çıksın
+            FormBorderStyle = FormBorderStyle.FixedDialog; //Percere sabit boyutlu.
+            MaximizeBox = false; //Sabit boyut - Sağ üsteki pencere büyütücü inaktif
+
+            _languageLabel = new Label
+            {
+                Left = 20,
+                Top = 15,
+                Width = 50,
+                AutoSize = true, //Label'ın genişliği/yüksekliği, içindeki yazıya göre kendini otomatik ayarlasın
+            };
+
+            _languageComboBox = new ComboBox
+            {
+                Left = 80,
+                Top = 12,
+                Width = 150,
+                DropDownStyle = ComboBoxStyle.DropDownList,
+            };
+
+            _languageComboBox.Items.Add("Türkçe"); //0
+            _languageComboBox.Items.Add("English"); //1
+            _languageComboBox.Items.Add("Deutsch"); //2
+            _languageComboBox.SelectedIndex = 0;
+            _languageComboBox.SelectedIndexChanged += OnLanguageChanged; // kullanıcı listeden birşey seçtiğinde OnLanguageChanged metodu çalışacak
+
+
+
+
+
         }
 
 
